ccac9f6a43b4903a278f5ab724e30ea13475c02e
Moshe Malka <moshemal@gmail.com>
2019-02-07 19:38:54 +0200

    add LineReader

---
+++ src/LineReader.js
@@ -0,0 +1,34 @@
+import ChunkReader from './ChunkReader';
+
+function LineReader(file) {
+  const chunkReader = new ChunkReader(file);
+  const lines = [];
+  let lastChunk = '';
+  Object.assign(this, {
+    read: (n = 1) => {
+      if (lines.length > n) {
+        return Promise.resolve(lines.splice(0, n)); 
+      } else if (chunkReader.hasNext()) {
+        return chunkReader.read().then(chunk => {
+          chunk = lastChunk + chunk;
+          const newLines = chunk.split('\n');
+          lastChunk = newLines.pop();
+          lines.push(...newLines);
+          return this.read(n);
+        });
+      } else {
+        if(lastChunk){
+          lines.push(lastChunk);
+          lastChunk = '';
+        }
+        return Promise.resolve(lines);
+      }
+    },
+
+    hasNext: () => {
+      return lines.length > 0 || chunkReader.hasNext();
+    }
+  });
+}
+
+export default LineReader;
