153eef99cf60761fe53fd52b9bab1b72790255cb
Moshe Malka <moshemal@gmail.com>
2019-02-26 19:09:30 +0200

    test lineReader

--- src/LineReader.test.js
+++ src/LineReader.test.js
@@ -13,3 +13,28 @@ describe('Instance creation', () => {
     expect(typeof lr[name]).toBe('function');
   });
 });
+
+describe('reading 1 chars file', () => {
+  const buffer = ['a'];
+  const file = new File(buffer, 'bla.txt');
+  
+  it('reading first line with should return "a"', async () => {
+    const lr = new LineReader(file);
+    const l = await lr.readLine();
+    expect(l).toBe('a');
+    expect(lr.hasNext()).toBeFalsy();
+  });
+});
+
+describe('reading 2 lines file', () => {
+  const buffer = ['a\nb'];
+  const file = new File(buffer, 'bla.txt');
+  
+  it('should have exactly 2 lines "a" and "b"', async () => {
+    const lr = new LineReader(file);
+    expect(await lr.readLine()).toBe('a');
+    expect(lr.hasNext()).toBe(true);
+    expect(await lr.readLine()).toBe('b');
+    expect(lr.hasNext()).toBeFalsy();
+  });
+});
